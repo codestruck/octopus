a3148a0ffd3e6122fc2dc9f72524ed6d23a23b4d
Matt Hinz <matthinz@gmail.com>
2011-03-23 14:27:56 -0700

    - u() can now change, replace, or remove the querystring of the url it is passed.

--- includes/functions/http.php
+++ includes/functions/http.php
@@ -5,31 +5,71 @@
     /**
      * Assembles a URL, ensuring it is properly prefixed etc.
      * @param $url string URL to format.
-     * @param $args array A new querystring for the URL.
+     * @param $args array Any querystring arguments to add to the URL. By
+     * default, this is processed as changes to the current querystring. Setting
+     * a key to null will remove it from the querystring. To overwrite $url's
+     * current querystring, set the 'replace_qs' option in $options.
      * @param $options array Options (mostly for testing).
      * @return A nice URL.
      */
     function u($url, $args = null, $options = null) {
-        
-        $url_base = defined('URL_BASE') ? URL_BASE : false;
-        if ($options && isset($options['URL_BASE'])) {
-            $url_base = $options['URL_BASE'];
+
+        // Prepend URL_BASE if it's not e.g. an http:// url
+        if (!preg_match('#^[a-z0-9]+://#i', $url)) {
+
+            $url_base = defined('URL_BASE') ? URL_BASE : false;
+
+            if ($options && isset($options['URL_BASE'])) {
+                $url_base = $options['URL_BASE'];
+            }
+
+            if ($url_base) {
+
+                if (strncmp($url, '/', 1) == 0) {
+                    // It's an absolute path, so prepend URL_BASE
+                    $url = $url_base . ltrim($url, '/');
+                }
+
+            }
         }
-        
-        if (!$url_base) {
+
+        if ($args === null) return $url;
+
+        // Process qs changes
+        $qPos = strpos($url, '?');
+        $qs = '';
+        if ($qPos !== false) {
+            $qs = substr($url, $qPos + 1);
+            $url = substr($url, 0, $qPos);
+        }
+
+        if ($options && isset($options['replace_qs']) && $options['replace_qs']) {
+            // Replace the current querystring
+            $qs = http_build_query($args);
+            if ($qs) $url .= '?' . $qs;
             return $url;
         }
 
-        if (preg_match('#^[a-z]+://#i', $url)) {
-            return $url;
+        // Apply changes to the querystring.
+        $oldArgs = array();
+        parse_str($qs, $oldArgs);
+
+        foreach($args as $key => $value) {
+
+            if ($value === null) {
+                unset($oldArgs[$key]);
+                continue;
+            }
+
+            $oldArgs[$key] = $value;
         }
-        
-        if (strncmp($url, '/', 1) == 0) {
-            // It's an absolute path, so prepend URL_BASE
-            return $url_base . ltrim($url, '/');
+
+        $qs = http_build_query($oldArgs);
+
+        if ($qs) {
+            $url .= '?' . $qs;
         }
-        
-        // It is a relative path, so keep in tact.
+
         return $url;
     }
 
@@ -40,28 +80,28 @@
         global $__SG_CANCEL_REDIRECT__;
         $__SG_CANCEL_REDIRECT__ =  $cancel;
     }
-    
+
     /**
-     * @return mixed The base path for the site, off which all URLs should be 
+     * @return mixed The base path for the site, off which all URLs should be
      * built. If the path can't be determined, returns false.
      * @param $rootDir string ROOT_DIR value to use, defaulting to ROOT_DIR.
      * @param $documentRoot string Document root to use when calculating. Defaults to $_SERVER['DOCUMENT_ROOT']
      */
     function find_url_base($rootDir = null, $documentRoot = null) {
-        
+
         $rootDir = $rootDir ? $rootDir : ROOT_DIR;
         $documentRoot = $documentRoot ? $documentRoot : $_SERVER['DOCUMENT_ROOT'];
-        
+
         if (!$documentRoot) {
             return '/'; // probably testing or on command line
         }
-        
+
         /*
          * Typical cases:
-         * 
+         *
          * $documentRoot = /var/www/
          * $rootDir = /var/www/
-         * result = '/' 
+         * result = '/'
          *
          * $documentRoot = /var/www/
          * $rootDir = /var/www/subdir/
@@ -70,23 +110,23 @@
          * $documentRoot = /var/www/
          * $rootDir = /some/weird/dir/
          * result = false
-         * 
+         *
          */
-        
+
         if (strncasecmp($rootDir, $documentRoot, strlen($documentRoot)) == 0) {
-            
-            
+
+
             $base = substr($rootDir, strlen($documentRoot));
             if ($base === false) return '/';
-            
+
             return start_in('/', end_in('/', $base));
-            
+
         } else {
             // Something weird is going on
             return false;
         }
     }
-    
+
     /**
      * Verbose alias for u().
      */
@@ -100,31 +140,31 @@
     function moved_permanently($newLocation) {
         redirect($newLocation, true);
     }
-    
+
     /**
      * Does an HTTP redirect.
      */
     function redirect($newLocation, $permanent = true) {
-        
+
         $newLocation = u($newLocation);
-        
+
         if (should_redirect()) {
             header($permanent ? 'HTTP/1.1 301 Moved Permanently' : 'HTTP/1.1 302 Found');
             header('Location: ' . u($newLocation));
         } else {
             // TODO: log?
         }
-        
+
         exit();
     }
-    
+
     /**
      * Reloads the current page.
      */
     function reload() {
         redirect($_SERVER['REQUEST_URI']);
     }
-    
+
     /**
      * @return bool Whether you should process a redirect.
      */
--- tests/functions/HttpTest.php
+++ tests/functions/HttpTest.php
@@ -5,52 +5,112 @@
      */
     class HttpTests extends PHPUnit_Framework_TestCase
     {
-        
+
         function testMakeUrl() {
-            
+
             $this->assertEquals(
                 'foo',
                 u('foo'),
                 'should keep relative paths in tact!'
             );
-            
+
             $this->assertEquals(
                 'http://www.google.com',
                 u('http://www.google.com'),
                 'should keep full urls in tact'
             );
-            
+
             $this->assertEquals(
                 '/test/foo',
                 u('/foo', null, array('URL_BASE' => '/test/')),
                 'Should prepend URL_BASE to absolute paths.'
             );
-            
+
         }
-        
+
         function testFindUrlBase() {
-            
+
             $this->assertEquals(
                 '/',
                 find_url_base('/var/www/', '/var/www/'),
                 'when doc_root == root_dir, base should be slash'
             );
-            
+
             $this->assertEquals(
                 '/subdir/',
                 find_url_base('/var/www/subdir/', '/var/www/'),
                 'should detect when running in subdir.'
             );
-            
+
             $this->assertEquals(
                 false,
                 find_url_base('/var/www/', '/some/weird/path/'),
                 'Should return false when not running off document_root'
             );
-                
-            
+
+
         }
-    
+
+        function testChangeUrlQueryString() {
+
+            foreach(array('/', 'http://') as $prefix) {
+
+                $url = "{$prefix}whatever?foo=bar&action&unchanged=1&q=matt&shoulddisappear=3";
+                $this->assertEquals(
+                    "{$prefix}whatever?foo=baz&action=search&unchanged=1&q=hinz",
+                    u(
+                        $url,
+                        array(
+                            'foo' => 'baz',
+                            'action' => 'search',
+                            'q' => 'hinz',
+                            'shoulddisappear' => null
+                        )
+                    ),
+                    "prefix: $prefix"
+                );
+            }
+
+        }
+
+        function testOverwriteUrlQueryString() {
+
+            foreach(array('/', 'http://') as $prefix) {
+
+                $url = "{$prefix}whatever?foo=bar&action&unchanged=1&q=matt&shoulddisappear=3";
+                $this->assertEquals(
+                    "{$prefix}whatever?foo=baz&action=search&q=hinz",
+                    u(
+                        $url,
+                        array(
+                            'foo' => 'baz',
+                            'action' => 'search',
+                            'q' => 'hinz',
+                            'shoulddisappear' => null
+                        ),
+                        array('replace_qs' => true)
+                    ),
+                    "prefix: $prefix"
+                );
+            }
+
+        }
+
+        function testRemoveUrlQueryString() {
+
+            foreach(array('/', 'http://') as $prefix) {
+
+                $url = "{$prefix}whatever?foo=bar&action&unchanged=1&q=matt&shoulddisappear=3";
+                $this->assertEquals(
+                    "{$prefix}whatever",
+                    u($url, array(), array('replace_qs' => 1)),
+                    "prefix: $prefix"
+                );
+            }
+
+
+        }
+
     }
 
 ?>
