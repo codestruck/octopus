c1b7df7100d625cfb88afb53f259313c69d211c2
Matt Hinz <matthinz@gmail.com>
2011-03-23 17:22:10 -0700

    - Set default time zone if not already set (like on hinz's machine)  - Added dump_r redirect canceling demo to index.php

--- index.php
+++ index.php
@@ -3,7 +3,20 @@
     require_once('includes/core.php');
 
     bootstrap();
-    
+
     echo "Congrats! You've got Project Octopus Running!";
-    
+
+    echo "<p>";
+    echo a('?dump_r=1', "Test dump_r redirect canceling");
+    echo "</p>";
+
+    if (get_numeric('dump_r')) {
+
+        $myVar = array('test variable' => true);
+        dump_r($myVar);
+
+        redirect('should_be_canceled');
+    }
+
 ?>
+
