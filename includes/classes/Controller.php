ef235c747e87d1169097e0e8d12575e757810f0f
Matt Hinz <matthinz@gmail.com>
2011-04-01 14:52:28 -0700

    - Beginnings of controller

--- includes/classes/Controller.php
+++ includes/classes/Controller.php
@@ -1,7 +1,38 @@
 <?php
 
-class SG_Controller {
+/**
+ * Base class for implementing an Octopus controller.
+ */
+abstract class SG_Controller {
 
+    /**
+     * If the action specified does not exist on this class, defaultAction()
+     * gets called.
+     */
+    public function defaultAction() {
+        return $this->render();
+    }
+
+    /**
+     * Called at the end of each action to render the view HTML.
+     * @return String View html.
+     */
+    public function render($data = array(), $options = null) {
+    }
+
+    /**
+     * Called at the end of each action to render the controller data as
+     * JSON.
+     */
+    public function renderJson($data = array(), $options = null) {
+
+        header('Content-type: application/json');
+
+        echo json_encode($data);
+
+        exit();
+
+    }
 
 
 }
