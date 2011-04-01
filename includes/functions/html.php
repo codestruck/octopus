9aaebd922c44295018d7ce4df9a89b58f9d476ba
Matt Hinz <matthinz@gmail.com>
2011-04-01 14:51:45 -0700

    - Add get_jquery_url helper to return a jquery cdn url

--- includes/functions/html.php
+++ includes/functions/html.php
@@ -8,4 +8,16 @@
         return "<a href=\"$href\">$text</a>";
     }
 
+    /**
+     * @return string URL to a CDN-hosted jQuery
+     */
+    function get_jquery_url($version = '1.5', $secure = null) {
+
+        if ($secure === null) {
+            $secure = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on';
+        }
+
+        return ($secure ? 'https' : 'http') . '://ajax.googleapis.com/ajax/libs/jquery/' . $version . '/jquery.min.js';
+    }
+
 ?>
