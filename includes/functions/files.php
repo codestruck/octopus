fe70431204caa1fb4c591b8abdb7f1ddb426e2ea
Matt Hinz <matthinz@gmail.com>
2011-04-03 22:00:46 -0700

    - Rework get_file to support configurable directories, multiple extensions, and an option to return the newest matching file.

--- includes/functions/files.php
+++ includes/functions/files.php
@@ -1,33 +1,117 @@
 <?php
 
     /**
-     * Checks the sitedir for a file, returning its path. If the file is not
-     * found in the sitedir, returns a path to the file in core.
-     * @return string The full path to a file.
+     * Looks for a file.
+     *
+     * @param $paths Mixed Path to the file, relative to the directories being
+     * searched. Can also be an array.
+     *
+     * @param $dirs Array Directories to search. Defaults to SITE_DIR and
+     * OCTOPUS_DIR.
+     *
+     * @param $options Array Any additional options.
+     *
+     * @return Mixed The full path to a file, or false if the file isn't found.
      */
-    function get_file($path, $options = null) {
+    function get_file($paths, $dirs = null, $options = null) {
 
-        $rootDir = ROOT_DIR;
-        $siteDir = SITE_DIR;
+        if ($dirs === null) {
 
-        if ($options) {
+            $octopusDir = OCTOPUS_DIR;
+            $siteDir = SITE_DIR;
 
-            if (!empty($options['ROOT_DIR'])) $rootDir = rtrim($options['ROOT_DIR'], '/') . '/';
-            if (!empty($options['SITE_DIR'])) $siteDir = rtrim($options['SITE_DIR'], '/') . '/';
+            if ($options) {
+
+                if (!empty($options['OCTOPUS_DIR'])) $rootDir = $options['OCTOPUS_DIR'];
+                if (!empty($options['SITE_DIR'])) $siteDir = $options['SITE_DIR'];
+
+            }
+
+            $dirs = array($siteDir, $octopusDir);
 
         }
 
-        $path = ltrim($path, '/');
+        $options = ($options === null ? array() : $options);
+        if (!isset($options['newest'])) $options['newest'] = false;
+        if (!isset($options['extensions'])) $options['extensions'] = false;
+        if (!isset($options['debug'])) $options['debug'] = false;
 
-        if (file_exists($siteDir . $path)) {
-            return $siteDir . $path;
+        if (!DEV) $options['debug'] = false;
+
+        if (!is_array($paths)) {
+            $paths = array($paths);
         }
 
-        if (file_exists($rootDir . $path)) {
-            return $rootDir . $path;
+        foreach($paths as $path) {
+
+            $path = ltrim($path, '/');
+            $newestTime = 0;
+            $newestPath = false;
+            $found = false;
+
+
+            foreach($dirs as &$dir) {
+
+                $dir = rtrim($dir, '/') . '/';
+                $fullPath = $dir . $path;
+
+                if (empty($options['extensions'])) {
+
+                    if ($options['debug']) {
+                            echo '<pre>' . htmlspecialchars($fullPath) . '</pre>';
+                        }
+
+                    $found = _get_file_helper($fullPath, $options, $newestTime, $newestPath);
+                    if ($found && !$options['newest']) {
+                        return $found;
+                    }
+
+                } else {
+
+                    foreach($options['extensions'] as $ext) {
+
+                        if ($options['debug']) {
+                            echo '<pre>' . htmlspecialchars($fullPath . $ext) . '</pre>';
+                        }
+
+                        $found = _get_file_helper($fullPath . $ext, $options, $newestTime, $newestPath);
+                        if ($found && !$options['newest']) {
+                            return $found;
+                        }
+                    }
+
+                }
+            }
+
         }
 
-        return false;
+        // TODO: Search modules for file
+
+        if ($options['newest']) {
+            return $newestPath;
+        } else {
+            return $found;
+        }
+
+    }
+
+    function _get_file_helper($path, $options, &$newestTime, &$newestPath) {
+
+        if (!file_exists($path)) {
+            return false;
+        }
+
+        if ($options['newest']) {
+
+            $time = filemtime($path);
+            if ($time > $newestTime) {
+                $newestTime = $time;
+                $newestPath = $path;
+            }
+
+        }
+
+        return $path;
     }
 
     /**
