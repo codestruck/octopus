f301efca51e154b2eb6402480b409777659a64b4
Matt Hinz <matthinz@gmail.com>
2011-04-10 21:23:12 -0700

    - 1st version of a Smarty renderer

--- includes/classes/Renderer/Smarty.php
+++ includes/classes/Renderer/Smarty.php
@@ -1,9 +1,23 @@
 <?php
 
+SG::loadExternal('smarty');
+
 class SG_Renderer_Smarty extends SG_Renderer {
 
     public function render($data) {
-        die("SG_Renderer_Smarty not implemented.");
+
+        $smarty = SG_Smarty::singleton()->smarty;
+        $smartyData = $smarty->createData();
+
+        if (is_array($data)) {
+            foreach($data as $key => $value) {
+                $smartyData->assign($key, $value);
+            }
+        }
+
+        $tpl = $smarty->createTemplate($this->_file, $smartyData);
+        return $tpl->fetch();
+
     }
 
 }
