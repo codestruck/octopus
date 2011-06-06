1f72675a4a332ffdc700058d4ce0144b5c4668ec
Matt Hinz <matthinz@gmail.com>
2011-06-06 14:15:56 -0700

    - Wrap fuzzy_* functions in ifs to avoid conflicts in solecms

--- includes/functions/dates.php
+++ includes/functions/dates.php
@@ -2,74 +2,80 @@
 
 define("FUZZY_ONE_DAY", 86400);
 
-function fuzzy_time($time = 0)
-{
-    if($time == 0)
+if (!function_exists('fuzzy_time')) {
+
+    function fuzzy_time($time = 0)
     {
-        return '-';
-    }
-
-    if(!$time)
-    {
-        return FALSE;
-    }
-
-    if (!is_numeric($time)) {
-        $time = strtotime($time);
-    }
-
-    $now = time();
-
-    // sod = start of day
-    $sodTime = mktime(0, 0, 0, date("m", $time), date("d", $time), date("Y", $time));
-    $sodNow  = mktime(0, 0, 0, date("m", $now), date("d", $now), date("Y", $now));
-
-    $diff = abs($sodNow - $sodTime);
-
-    // check 'today'
-    if ($sodNow == $sodTime)
-    {
-        return "Today at ".date("g:ia", $time);
-    }
-
-    // check 'yesterday'
-    if ($diff <= FUZZY_ONE_DAY)
-    {
-        if ($sodTime > $sodNow) {
-            return "Tomorrow at ".date("g:ia", $time);
-        } else {
-            return "Yesterday at ".date("g:ia", $time);
+        if($time == 0)
+        {
+            return '-';
         }
-    }
 
-    // give a day name if within the last 5 days
-    if($diff <= (FUZZY_ONE_DAY * 5))
-    {
-        return date("D \a\\t g:ia", $time);
-    }
+        if(!$time)
+        {
+            return FALSE;
+        }
 
-    // miss off the year if it's this year
-    if(date("Y", $now) == date("Y", $time))
-    {
-        return date("M j \a\\t g:ia", $time);
-    }
+        if (!is_numeric($time)) {
+            $time = strtotime($time);
+        }
 
-    // return the date as normal
-    return date("M j, Y \a\\t g:ia", $time);
+        $now = time();
+
+        // sod = start of day
+        $sodTime = mktime(0, 0, 0, date("m", $time), date("d", $time), date("Y", $time));
+        $sodNow  = mktime(0, 0, 0, date("m", $now), date("d", $now), date("Y", $now));
+
+        $diff = abs($sodNow - $sodTime);
+
+        // check 'today'
+        if ($sodNow == $sodTime)
+        {
+            return "Today at ".date("g:ia", $time);
+        }
+
+        // check 'yesterday'
+        if ($diff <= FUZZY_ONE_DAY)
+        {
+            if ($sodTime > $sodNow) {
+                return "Tomorrow at ".date("g:ia", $time);
+            } else {
+                return "Yesterday at ".date("g:ia", $time);
+            }
+        }
+
+        // give a day name if within the last 5 days
+        if($diff <= (FUZZY_ONE_DAY * 5))
+        {
+            return date("D \a\\t g:ia", $time);
+        }
+
+        // miss off the year if it's this year
+        if(date("Y", $now) == date("Y", $time))
+        {
+            return date("M j \a\\t g:ia", $time);
+        }
+
+        // return the date as normal
+        return date("M j, Y \a\\t g:ia", $time);
+    }
 }
 
-function fuzzy_date($time = 0)
-{
-    $text = fuzzy_time($time);
+if (!function_exists('fuzzy_date')) {
 
-    if(!$text)
+    function fuzzy_date($time = 0)
     {
-        return FALSE;
+        $text = fuzzy_time($time);
+
+        if(!$text)
+        {
+            return FALSE;
+        }
+
+        $pieces = explode(" at ", $text);
+
+        return $pieces[0];
     }
-
-    $pieces = explode(" at ", $text);
-
-    return $pieces[0];
 }
 
 ?>
