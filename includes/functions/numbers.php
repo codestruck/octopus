5437b6cd5760d2f0159aa90a183947dd952f0dbf
Matt Hinz <matthinz@gmail.com>
2011-06-09 16:17:35 -0700

    - Add html version of format_money

--- includes/functions/numbers.php
+++ includes/functions/numbers.php
@@ -6,7 +6,27 @@
      * Formats a dollar amount.
      */
     function format_money($amount, $format = null) {
-        return money_format('%(.2n', $amount);
+
+        if (!is_string($format)) {
+            $format = '%(.2n';
+        }
+
+        return money_format($format, $amount);
+    }
+
+    /**
+     * Formats a dollar amount, wrapping in a <span class="negative"></span>
+     * if the amount is negative.
+     */
+    function html_format_money($amount, $format = null) {
+
+        $formatted = htmlspecialchars(format_money($amount, $format));
+
+        if ($amount < 0) {
+            return '<span class="negative">' . $formatted . '</span>';
+        }
+
+        return $formatted;
     }
 
 ?>
