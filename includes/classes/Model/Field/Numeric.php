086ce702b20d603c5ad5ef7aa689fc93b5c126de
Matt Hinz <matthinz@gmail.com>
2011-07-20 11:04:00 -0700

    - Allow saving negative numeric values :-)

--- includes/classes/Model/Field/Numeric.php
+++ includes/classes/Model/Field/Numeric.php
@@ -34,7 +34,7 @@ class Octopus_Model_Field_Numeric extends Octopus_Model_Field {
 
     public function save($model, $sqlQuery) {
         $value = $this->accessValue($model, true);
-        $value = preg_replace('/[^\d\.]/', '', $value);
+        $value = preg_replace('/[^\d\.-]/', '', $value);
         $sqlQuery->set($this->getFieldName(), $value);
     }
 
