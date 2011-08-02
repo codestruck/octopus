18f5204ca8bad25f9353c6c0a3d17f23d084a5d5
Matt Hinz <matthinz@gmail.com>
2011-08-02 13:25:12 -0700

    - Clean up dump_r's html output, add Dumpable interface

--- includes/functions/debug.php
+++ includes/functions/debug.php
@@ -28,6 +28,21 @@ function output_dumped_content_header($data, $response) {
 
 }
 
+// Dumpable interface {{{
+
+interface Dumpable {
+
+    /**
+     * Returns debug info on this object.
+     * @param $mode String 'html' or 'text'
+     * @return String dumped content.
+     */
+    function dump($mode);
+
+}
+
+// }}}
+
 // Octopus_Debug Class {{{
 
 /**
@@ -36,11 +51,16 @@ function output_dumped_content_header($data, $response) {
 class Octopus_Debug {
 
     public static $css;
+    public static $js;
     private $_id;
     private $_content = array();
     private $_options;
+    private $_footer;
+
     private static $_renderedCss = false;
+    private static $_renderedJs = false;
     private static $_idCounter = 0;
+
 
     private static function getNewId() {
         return "sgDebug" . (++self::$_idCounter);
@@ -48,71 +68,188 @@ class Octopus_Debug {
 
     public function __construct($id) {
         $this->_id = $id;
+
+        self::$js = <<<END
+<script type="text/javascript">
+
+function __octopus_getElement(id) {
+
+    if (document.getElementById) {
+        return document.getElementById(id);
+    } else if (document.all) {
+        return document.all(id);
+    }
+}
+
+function __octopus_jQuery_exists() {
+    return typeof($) !== 'undefined' && typeof($.fn) !== 'undefined' && typeof($.fn.show) !== 'undefined';
+}
+
+function __octopus_show(id) {
+
+    if (__octopus_jQuery_exists()) {
+        $('#' + id).show('fast');
+        return;
+    }
+
+    var e = __octopus_getElement(id);
+
+    if (e) {
+        e.style.display = '';
+    }
+
+}
+
+function __octopus_hide(id) {
+
+    if (__octopus_jQuery_exists()) {
+        $('#' + id).hide('fast');
+        return;
+    }
+
+    var e = __octopus_getElement(id);
+
+    if (e) {
+        e.style.display = '';
+    }
+}
+
+function __octopus_toggle(id) {
+
+    if (__octopus_jQuery_exists()) {
+        $('#' + id).toggle('fast');
+        return;
+    }
+
+    var e = __octopus_getElement(id);
+
+    if (e) {
+
+        if (/^none$/i.test(e.style.display)) {
+            e.style.display = '';
+        } else {
+            e.style.display = 'none';
+        }
+
+    }
+}
+
+function __octopus_openTab(id, buttonID) {
+
+    var tab = __octopus_getElement(id);
+    if (!tab) return;
+
+    for(var e = tab.parentNode.firstChild; e; e = e.nextSibling) {
+
+        if (e === tab) {
+            e.style.display = '';
+        } else {
+            e.style.display = 'none';
+        }
+
+    }
+
+    var button = __octopus_getElement(buttonID);
+    if (button) {
+        for(var e = button.parentNode.firstChild; e; e = e.nextSibling) {
+
+            if (e === button) {
+                e.className = e.className.replace(/sgDebugTabButtonSelected/g, '') + ' sgDebugTabButtonSelected';
+            } else {
+                e.className = e.className.replace(/sgDebugTabButtonSelected/g, '');
+            }
+        }
+    }
+
+
+}
+
+</script>
+END;
+
         self::$css = <<<END
 
 <style type="text/css">
 <!--
 
-    .sgDebug {
-        background: #ccc;
-        border: 1px solid #333;
+    div.sgDebug * {
+        font-size: 1em;
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+
+    div.sgDebug {
+        background-color: #efefef;
+        border: 1px solid #888;
         color: #000;
-        font-family: monospace;
-        margin: 0 0 10px 0;
+        font-size: 12px;
+        font-family: 'Monaco', 'Andale Mono', 'Consolas', 'Courier New', monospace;
+        margin-bottom: 10px;
+        max-width: 600px;
+        padding: 0;
+        width: 50%;
+    }
+
+    div.sgDebug div.sgDebugTabs {
+    }
+
+    div.sgDebug div.sgDebugTabs div.sgDebugTab {
+        max-height: 300px;
+        overflow: auto;
         padding: 10px;
     }
 
-    table.sgDebugContentTable {
-        border-spacing: 10px;
+    div.sgDebug ul.sgDebugTabButtons {
+        background: #ccc;
+        overflow: hidden;
     }
 
-    table.sgDebugContentTable tr td {
-        background: #DDD;
-        border: 1px solid #333;
-        padding: 5px;
+    div.sgDebug ul.sgDebugTabButtons li {
+        float: left;
+    }
+
+    div.sgDebug ul.sgDebugTabButtons li a {
+        color: #333;
+        display: block;
+        padding: 5px 10px;
+        text-decoration: none;
+    }
+
+    div.sgDebug ul.sgDebugTabButtons li.sgDebugTabButtonSelected a {
+    background: #efefef;
+    }
+
+    .sgDebugErrorReporting li {
+        float: right;
+        list-style: none;
+        margin-left: 20px;
+    }
+
+    div.sgDebug table {}
+    div.sgDebug table tr td {
+        padding: 2px;
+    }
+    div.sgDebug table tr.sgDebugOdd td {
+        background: #fff;
+    }
+    div.sgDebug table tr th {
         text-align: left;
-        vertical-align: top;
-    }
-    table.sgDebugContentTable tr td.sgDebugFirst { text-align: left; }
-
-
-    .sgDebug_var {
-        margin-bottom: 10px;
     }
 
-    table.sgDebugArrayTable {
-        background: #eee;
-        margin: 5px 0;
-    }
-    table.sgDebugArrayTable tr td, table.sgDebugArrayTable tr th {
-        text-align: left;
+    table.sgDebugArrayDump {
     }
 
-    .sgDebugMissingArray { color: #444; }
-
-    ul.sgDebugBacktrace {
-        list-style-type: none;
-        padding: 0;
+    table.sgDebugArrayDump td.sgDebugArrayKey {
+        color: #555;
+        padding-right: 10px;
     }
-    ul.sgDebugBacktrace li.sgDebugFirst { font-weight: bold; }
 
-
-    table.sgDebug_footer {
+    table.sgDebugBacktrace {
         width: 100%;
     }
-    table.sgDebug_footer tr td {
-        background: none;
-        border: none;
-    }
 
-    .sgDebugErrorReporting {
-        border-top: 1px dotted #555;
-        color: #555;
-        font-size: 0.9em;
-        margin-top: 10px;
-        padding-top: 10px;
-        text-align: right;
-    }
+
 
 -->
 </style>
@@ -121,12 +258,11 @@ END;
     }
 
     public function add($name, $content) {
+        $this->_content[$name] = $content;
+    }
 
-        if (!isset($this->_content[$name])) {
-            $this->_content[$name] = array();
-        }
-
-        $this->_content[$name][] = $content;
+    public function setFooter($content) {
+        $this->_footer = $content;
     }
 
     public function render($return = false) {
@@ -146,6 +282,11 @@ END;
         if (!self::$_renderedCss || $return) {
             $result .= self::$css;
             if (!$return) self::$_renderedCss = true;
+        }
+
+        if (!self::$_renderedJs || $return) {
+            $result .= self::$js;
+            if (!$return) self::$_renderedJs = true;
         }
 
         $content = $this->getContentHtml();
@@ -172,41 +313,58 @@ END;
             return '';
         }
 
+        $buttons = '<ul class="sgDebugTabButtons">';
+        $tabs = '<div class="sgDebugTabs">';
+
         $result = '';
+        $index = 0;
         foreach($this->_content as $name => $values) {
-            $html = self::buildContentTable($name, $values);
-            $result .= $html;
+
+            $id = preg_replace('/[^a-z0-9_-]/i', '_', $name);
+            $safeName = htmlspecialchars($name);
+
+            $buttonClass = 'sgDebugTabButton';
+            if ($index === 0) $buttonClass .= ' sgDebugTabButtonSelected';
+
+            $buttonID = self::getNewId();
+
+            $buttons .= <<<END
+<li id="$buttonID" class="$buttonClass"><a href="#" onclick="__octopus_openTab('$id', '$buttonID'); return false;">$safeName</a></li>
+END;
+            $tabs .= self::buildContentSection($id, $name, $values, $index, $count);
+
+            $index++;
         }
 
-        return $result;
+        $buttons .= '</ul>';
+        $tabs .= '</div>';
+        $footer = '';
+
+        if ($this->_footer) {
+            $footer = <<<END
+<
+END;
+        }
+
+        return $tabs . $buttons;
 
     }
 
-    private static function buildContentTable($name, $values) {
+    private static function buildContentSection($id, $name, $content, $index, $count) {
+
+        $sectionClass = 'sgDebugTab';
+        if ($index === 0) $sectionClass .= ' sgDebugFirst';
+        if ($index === $count - 1) $sectionClass .= ' sgDebugLast';
+
+        $styleAttr = '';
+        if ($index !== 0) $styleAttr = ' style="display:none;"';
 
         $html = <<<END
-<table class="sgDebugContentTable sgDebug_$name">
-<tr>
+<div id="$id" class="$sectionClass"$styleAttr>
 END;
-        $count = count($values);
-        $width = 100.0 / $count;
 
-        $i = 0;
-        foreach($values as $v) {
-
-            $classAttr = '';
-            if ($i == 0) $classAttr = 'sgDebugFirst';
-            if ($i == count($values) - 1) $classAttr .= ' sgDebugLast';
-            $classAttr = $classAttr ? " class=\"$classAttr\"" : '';
-
-            $html .= "<td style=\"width: {$width}%;\" $classAttr>";
-            $html .= $v;
-            $html .= "</td>";
-
-            $i++;
-        }
-
-        $html .= '</tr></table>';
+        $html .= $content;
+        $html .= '</div>';
 
         return $html;
     }
@@ -216,11 +374,9 @@ END;
         $line = str_repeat('-', 80);
         $result = "\n$line";
 
-        foreach($this->_content as $name => $c) {
+        foreach($this->_content as $name => $text) {
 
-            foreach($c as $text) {
-                $result .= "\n$text";
-            }
+            $result .= "\n$text";
 
         }
         $result .= "\n$line";
@@ -317,47 +473,55 @@ END;
         $skipFunction = true;
         $first = true;
 
-        $html = '<ul class="sgDebugBacktrace">';
+        $id = self::getNewId();
+
+        $html = <<<END
+<table class="sgDebugBacktrace" border="0" cellpadding="0" cellspacing="0">
+<thead>
+    <tr>
+        <th class="sgDebugBacktraceFunction">Function</th>
+        <th class="sgDebugBacktraceFile">File</th>
+        <th class="sgDebugBacktraceLine">Line</th>
+    </tr>
+</thead>
+<tbody>
+END;
 
         $i = 0;
         $count = count($bt);
 
         foreach(self::saneBacktrace($bt) as $b) {
 
-            $class = '';
-            if ($i == 0) $class = ' sgDebugFirst';
-            if ($i == $count - 1) $class .= ' sgDebugLast';
-            $class = $class ? ' class="' . $class . '"' : '';
+            $class = ($i % 2 ? 'sgDebugOdd' : 'sgDebugEven');
 
-            $func = '';
-            if ($b['function']) {
-                $func = '<span class="sgDebugBacktraceFunction">' . $b['function'] . '()</span>';
+            $func = '<td class="sgDebugBacktraceFunction">' . $b['function'] . '()</td>';
 
-            }
+            $b['file'] = htmlspecialchars($b['file']);
 
-            $file = '';
-            if ($b['file']) {
-                $file = '<span class="sgDebugBacktraceFile">' . htmlspecialchars($b['file']) . '</span>';
-            }
+            $file = <<<END
+<td class="sgDebugBacktraceFile" title="{$b['file']}">
+END;
 
-            $line = '';
-            if ($b['line']) {
-                $line = '<span class="sgDebugBacktraceLine">Line ' . $b['line'] . '</span>';
-            }
+            $file .= htmlspecialchars($b['nice_file']);
+            $file .= '</td>';
+
+            $line = '<td class="sgDebugBacktraceLine">Line ' .
+                    (isset($b['line']) ? $b['line'] : '') .
+                    '</td>';
 
             $html .= <<<END
-            <li$class>
+            <tr class="$class">
                 $func
                 $file
                 $line
-            </li>
+            </tr>
 END;
 
             $i++;
 
         }
 
-        $html .= '</ul>';
+        $html .= '</tbody></table>';
 
         return $html;
     }
@@ -366,10 +530,13 @@ END;
 
         $flags = implode(' | ', self::getErrorReportingFlags());
 
+        $display_errors = ini_get('display_errors') ? 'on' : 'off';
+
         return <<<END
-        <div class="sgDebugErrorReporting">
-        error_reporting: $flags
-        </div>
+        <ul class="sgDebugErrorReporting">
+        <li>error_reporting: $flags</li>
+        <li>display_errors: $display_errors</li>
+        </ul>
 END;
 
 
@@ -378,59 +545,108 @@ END;
     /**
      * Returns HTML for the various system arrays
      */
-    public static function getArraysHtml() {
+    public static function getGlobalArrayHtml($arname) {
 
         $html = '';
 
-        foreach(array('$_GET', '$_POST', '$_SERVER', '$_SESSION') as $arname) {
+       eval("\$ar = isset($arname) ? $arname : false;");
+       if ($ar === false) {
+           $html .= '<span class=\"sgDebugMissingArray">' . $arname . ' (unavailable)</span>';
+           return $html;
+       }
 
-           eval("\$ar = isset($arname) ? $arname : false;");
-           if ($ar === false) {
-               $html .= '<span class=\"sgDebugMissingArray">' . $arname . ' (unavailable)</span>';
-               continue;
-           }
+       $id = self::getNewId();
 
-           $id = self::getNewId();
+       if (empty($ar)) {
+           $html .= '<span class="sgDebugEmptyArrayName">' . $arname . ' (Empty)</span><br>';
+           continue;
+       }
 
-           if (empty($ar)) {
-               $html .= '<span class="sgDebugEmptyArrayName">' . $arname . ' (Empty)</span><br>';
-               continue;
-           }
-
-           $html .= "<a href=\"#$arname\" onclick=\"var c = document.getElementById('$id'); if (c.style.display == 'none') c.style.display = ''; else c.style.display = 'none'; return false;\">$arname</a><br />";
-           $html .= '<table id="' . $id . '" style="display: none;" class="sgDebugArrayTable">';
+       $html .= "<a href=\"#$arname\" onclick=\"var c = document.getElementById('$id'); if (c.style.display == 'none') c.style.display = ''; else c.style.display = 'none'; return false;\">$arname</a><br />";
+       $html .= '<table id="' . $id . '" style="display: none;" class="sgDebugArrayTable">';
 
 
-           foreach($ar as $key => $value) {
+       foreach($ar as $key => $value) {
 
-               $value = var_export($value, true);
+           $value = var_export($value, true);
 
-               $html .=
-                '<tr>' .
-                    '<th>' . htmlspecialchars($key) . '</th>' .
-                    '<td>=></td>' .
-                    '<td>' . htmlspecialchars(stripslashes($value)) . '</td>';
-                '</tr>';
+           $html .=
+            '<tr>' .
+                '<th>' . htmlspecialchars($key) . '</th>' .
+                '<td>=></td>' .
+                '<td>' . htmlspecialchars(stripslashes($value)) . '</td>';
+            '</tr>';
 
-           }
+       }
 
-           $html .= '</table>';
-
-
-        }
+       $html .= '</table>';
 
        return $html;
 
     }
 
-    public static function dumpToString($x) {
+    public static function dumpToString($x, $escapeHtml = false, $useDumpable = true) {
 
         // var_export chokes on recursive references, but var_dump doesn't.
         // of course, var_dump involves output buffering. yay.
 
+        if (function_exists('xdebug_call_class')) {
+            $escapeHtml = false;
+        }
+
+        if ($useDumpable) {
+
+            if (is_object($x) && $x instanceof Dumpable) {
+                $mode = Octopus_Debug::inWebContext() ? 'html' : 'text';
+                $content = $x->dump($mode);
+                return $escapeHtml ? htmlspecialchars($content) : $content;
+            }
+
+        }
+
+        if (Octopus_Debug::inWebContext() && is_array($x)) {
+            return self::dumpArrayToHtml($x);
+        }
+
         ob_start();
         var_dump($x);
-        return trim(ob_get_clean());
+        $content = trim(ob_get_clean());
+
+        return $escapeHtml ? htmlspecialchars($content) : $content;
+    }
+
+    private static function dumpArrayToHtml($ar) {
+
+        if (empty($ar)) {
+            ob_start();
+            var_dump($x);
+            $content = trim(ob_get_clean());
+            return htmlspecialchars($content);
+        }
+
+        $result = '<table class="sgDebugArrayDump" border="0" cellpadding="0" cellspacing="0">';
+
+        $i = 0;
+        foreach($ar as $key => $value) {
+
+            $rowClass = ($i % 2) ? 'sgDebugOdd' : 'sgDebugEven';
+
+            $key = htmlspecialchars($key);
+            $value = self::dumpToString($value, true);
+
+            $result .= <<<END
+<tr class="$rowClass">
+    <td class="sgDebugArrayKey">$key</td>
+    <td class="sgDebugArrayValue">$value</td>
+</tr>
+END;
+                $i++;
+
+        }
+
+        $result .= '</table>';
+
+        return $result;
     }
 
     public static function saneBacktrace($bt = null) {
@@ -443,7 +659,7 @@ END;
 
         foreach($bt as $b) {
 
-            $result[] = array(
+            $item = array(
 
                 'function' => isset($b['function']) ? $b['function'] : null,
                 'file' => isset($b['file']) ? $b['file'] : null,
@@ -451,6 +667,13 @@ END;
 
             );
 
+            if (defined('ROOT_DIR') && starts_with($item['file'], ROOT_DIR)) {
+                $item['nice_file'] = substr($b['file'], strlen(ROOT_DIR));
+            } else {
+                $item['nice_file'] = $item['file'];
+            }
+
+            $result[] = $item;
         }
 
         return $result;
@@ -508,19 +731,28 @@ END;
             $d = new Octopus_Debug('dump_r');
             foreach($args as $arg) {
 
-                $output = Octopus_Debug::dumpToString($arg);
-                if (!function_exists('xdebug_call_class')) {
-                    $output = htmlspecialchars($output);
-                }
+                $output = Octopus_Debug::dumpToString($arg, true);
 
                 $html = '<pre>' . $output . '</pre>';
-                $d->add('var', $html);
+                $d->add('Variable', $html);
             }
-            $d->add('context', Octopus_Debug::getArraysHtml());
-            $bt = debug_backtrace();
-            $d->add('context', Octopus_Debug::getBacktraceHtml($bt));
 
-            $d->add('footer', Octopus_Debug::getErrorReportingHtml());
+            $bt = debug_backtrace();
+            $d->add('Backtrace', Octopus_Debug::getBacktraceHtml($bt));
+
+            foreach(array('_GET', '_POST', '_SERVER', '_SESSION') as $arname) {
+
+                if (isset($GLOBALS[$arname]) && !empty($GLOBALS[$arname])) {
+                    $html = '<pre class="sgDebugArray">' .
+                            Octopus_Debug::dumpToString($GLOBALS[$arname], true) .
+                            '</pre>';
+                    $d->add('$' . $arname, $html);
+                }
+
+            }
+
+
+            $d->setFooter(Octopus_Debug::getErrorReportingHtml());
 
         } else {
 
