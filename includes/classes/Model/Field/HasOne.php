c6a8b365358b20aaf5574ba05025ef7531135d91
Matt Hinz <matthinz@gmail.com>
2011-08-12 19:14:06 -0700

    - Fix missing property notice in Field_HasOne

--- includes/classes/Model/Field/HasOne.php
+++ includes/classes/Model/Field/HasOne.php
@@ -21,7 +21,7 @@ class Octopus_Model_Field_HasOne extends Octopus_Model_Field {
             $dataField = 'item_id';
         }
 
-        $value = $model->$dataField;
+        $value = isset($model->$dataField) ? $model->$dataField : null;
 
         $result = new $class($value);
         $result->escaped = $model->escaped;
