9bcf86cf3dd82d63f7c5c7278422dcf34e809f8d
Matt Hinz <matthinz@gmail.com>
2011-08-20 10:11:29 -0700

    - Add html_image shim

---
+++ externals/smarty/plugins/function.html_image.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once(dirname(__FILE__) . '/function.image.php');
+
+/**
+ * Shim for supporting old {html_image} style calls. See function.image.php
+ */
+function smarty_function_html_image($params, $template) {
+	return smarty_function_image($params, $template);
+}
+
+?>
