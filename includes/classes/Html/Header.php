7cdde0665225f73b6849d535ba7d582d5eb3d358
Matt Hinz <matthinz@gmail.com>
2011-08-24 12:42:04 -0700

    - Revert to old Html_Header for the time being

--- includes/classes/Html/Header.php
+++ includes/classes/Html/Header.php
@@ -1,16 +1,13 @@
 <?php
 
-Octopus::loadClass('Octopus_Html_Page');
-
-/**
- * @deprecated
- */
 class Octopus_Html_Header extends Octopus_Base {
 
-    private $page;
-
-    public function __construct($page = null) {
-        $this->page = $page ? $page : Octopus_Html_Page::singleton();
+    function Octopus_Html_Header() {
+        $this->js = array();
+        $this->css = array();
+        $this->trackModified = true;
+        $this->baseDir = '';
+        $this->URL_BASE = URL_BASE;
     }
 
     public static function &singleton() {
@@ -18,32 +15,166 @@ class Octopus_Html_Header extends Octopus_Base {
     }
 
     /**
-     * @deprecated
+     *
+     * @return string CSS and JS HTML headers
      */
     function getHeader() {
-        return  $this->page->renderMeta(true) .
-                $this->page->renderCss(true) .
-                $this->page->renderLinks(true) .
-                $this->page->renderJavascript(true);
+
+        $output = "\n";
+        $output .= $this->getCssHeader();
+        $output .= $this->getJavascriptHeader();
+        return $output;
+
     }
 
+    /**
+     * Add Javascript include
+     *
+     * @param string $file name of javascript file
+     * @return void
+     */
     function addJavascript($file, $attributes = array()) {
-        $this->page->addJavascript($file, $attributes);
+        $this->js[$file] = $attributes;
     }
 
     function getJavascriptHeader() {
-        return $this->page->renderJavascript(true);
+
+        $headers = '';
+
+        foreach ($this->js as $file => $attributes) {
+
+            $headers .= '<script type="text/javascript" src="' . $this->getFileModifiedLink($file) . '"' . $this->buildAttributeString($attributes) . '></script>';
+            $headers .= "\n";
+
+        }
+
+        return $headers;
+
     }
 
+    /**
+     * Add CSS include
+     *
+     * @param string $file name of css file
+     * @return void
+     */
     function addCss($file, $attributes = array()) {
-        $this->page->addCss($file, $attributes);
+        $this->css[$file] = $attributes;
     }
 
     function getCssHeader() {
-        return $this->page->renderCss(true);
+
+        $headers = '';
+
+        foreach ($this->css as $file => $attributes) {
+
+            $headers .= '<link rel="stylesheet" type="text/css" href="' . $this->getFileModifiedLink($file) . '"' . $this->buildAttributeString($attributes) . ' />';
+            $headers .= "\n";
+
+        }
+
+        return $headers;
+
     }
 
-    
+    function getFileModified($file) {
+
+        if ($this->URL_BASE != '/') {
+            $file = preg_replace('|^' . $this->URL_BASE . '|', '/', $file);
+        }
+
+        $pos = strpos($file, '?');
+        if ($pos) {
+            $file = substr($file, 0, $pos);
+        }
+
+        $fullfile = ROOT_DIR . $file;
+
+        if ($this->baseDir) {
+            $basefullfile = ROOT_DIR . $this->baseDir . '/' . $file;
+            if (is_file($basefullfile)) {
+                $fullfile = $basefullfile;
+            }
+        }
+
+        $mtime = filemtime($fullfile);
+
+        return $mtime;
+    }
+
+    function getFileSrc($file) {
+
+        $rawFile = $file;
+
+        // get relative base file path ?
+        if ($this->URL_BASE != '/') {
+            $rawFile = preg_replace('|^' . $this->URL_BASE . '|', '/', $file);
+        }
+
+        $lastDot = strrpos($rawFile, '.');
+        $src = substr($rawFile, 0, $lastDot) . '_src' . substr($rawFile, $lastDot);
+
+        $fullSrc = ROOT_DIR . $src;
+
+        if ($this->baseDir && $rawFile[0] != '/') {
+            $fullSrc = ROOT_DIR . $this->baseDir . '/' . $src;
+        }
+
+        if (is_file($fullSrc)) {
+            $lastDot = strrpos($file, '.');
+            $src = substr($file, 0, $lastDot) . '_src' . substr($file, $lastDot);
+
+            return $src;
+        }
+
+        return $file;
+
+    }
+
+    function getFileModifiedLink($file) {
+
+        // allow remote urls to be used
+        if (substr($file, 0, 4) == 'http') {
+            return $file;
+        }
+
+        if ($this->trackModified) {
+
+            $srcFile = $this->getFileSrc($file);
+            $srcMtime = $this->getFileModified($srcFile);
+            $mtime = $this->getFileModified($file);
+
+            if ($srcMtime > $mtime) {
+                $file = $srcFile;
+                $mtime = $srcMtime;
+            }
+
+            $end = '';
+
+            $pos = strpos($file, '?');
+            if ($pos) {
+                $file = $file . '&amp;' . $mtime;
+            } else {
+                $file = $file . '?' . $mtime;
+            }
+
+        }
+
+        return u($file);
+    }
+
+    function buildAttributeString($attrs) {
+
+        $attributes = '';
+
+        foreach ($attrs as $attr => $value) {
+            $attributes .= " $attr=\"$value\"";
+        }
+
+        return $attributes;
+
+    }
+
     function useTinyMce() {
         $this->addJavascript('scripts/tiny_mce/tiny_mce.js');
         $this->addJavascript('scripts/tinyMceInit.js');
