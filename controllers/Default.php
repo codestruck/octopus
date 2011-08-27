c7943c947bd0c9116c8f123a64a2a721f67b6827
Matt Hinz <matthinz@gmail.com>
2011-08-26 17:21:12 -0700

    - Support sites defining their own DefaultController

--- controllers/Default.php
+++ controllers/Default.php
@@ -1,7 +1,8 @@
 <?php
 
 /**
- * Controller used when none is specified.
+ * Controller used when none is specified. If the site defines its own
+ * DefaultController, that will be used instead.
  */
 class DefaultController extends Octopus_Controller {
 
