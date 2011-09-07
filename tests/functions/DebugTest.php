57e973b4afd5a3dfdecf4021289cebc33775c962
Matt Hinz <matthinz@gmail.com>
2011-09-07 09:48:08 -0700

    - Make dump_r html test less finicky

--- tests/functions/DebugTest.php
+++ tests/functions/DebugTest.php
@@ -130,9 +130,12 @@ $js
 <!-- END dump_r Output }}} -->
 END;
 
+        $expected = preg_replace('/id="octopusDebug\d+"', '', $expected);
+        $actual =   preg_replace('/id="octopusDebug\d+"', '', $d->renderHtml(true));
+
         $this->assertHtmlEquals(
             $expected,
-            $d->renderHtml(true)
+            $actual
         );
 
     }
