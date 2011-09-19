9470125278d3d24c89a830c4ad850ed47e5c43d0
Mike Estes <mikejestes@gmail.com>
2011-09-19 12:42:41 -0700

    - fix tests on 5.2

--- tests/model/MatchingTest.php
+++ tests/model/MatchingTest.php
@@ -11,21 +11,21 @@ class MatchingTest extends Octopus_DB_TestCase {
 
     function testMatchingHasOneSql() {
 
-        $matt = FindPost::All()->matching('Hinz');
+        $matt = FindPost::all()->matching('Hinz');
         $this->assertEquals('SELECT * FROM find_posts WHERE (`find_posts`.`title` LIKE ?) OR (`find_posts`.`author_id` IN (SELECT `find_author_id` FROM `find_authors` WHERE `find_authors`.`name` LIKE ?))', $matt->getSql());
 
     }
 
     function testMatchingHasOneCount() {
 
-        $matt = FindPost::All()->matching('Hinz');
+        $matt = FindPost::all()->matching('Hinz');
         $this->assertEquals(3, count($matt));
 
     }
 
     function testTest() {
 
-        $matt = FindPost::All()->where('author.name', 'Matt Hinz');
+        $matt = FindPost::all()->where('author.name', 'Matt Hinz');
         $this->assertEquals('SELECT * FROM find_posts WHERE `find_posts`.`author_id` IN (SELECT `find_author_id` FROM `find_authors` WHERE `find_authors`.`name` = ?)', $matt->getSql());
         $this->assertEquals(3, count($matt));
 
