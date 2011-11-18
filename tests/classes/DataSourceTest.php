abe1a72ae4845bc5fbd30a1710b2b0e1752b2723
Matt Hinz <matthinz@gmail.com>
2011-11-18 09:48:48 -0800

    WIP on data source tests

--- tests/classes/DataSourceTest.php
+++ tests/classes/DataSourceTest.php
@@ -3,9 +3,9 @@
 Octopus::loadClass('Octopus_DataSource_Array');
 
 class DataSourceTest extends PHPUnit_Framework_TestCase {
-	
+
 	function testArraySort() {
-		
+
 		$array = array(
 			array('id' => 1, 'name' => 'joe blow'),
 			array('id' => 2, 'name' => 'jane blow'),
@@ -35,11 +35,11 @@ class DataSourceTest extends PHPUnit_Framework_TestCase {
 			array(array('id' => 2, 'name' => 'jane blow')),
 			$filtered->getItems()
 		);
-		
+
 	}
 
 	function testArraySortAndFilter() {
-		
+
 		$array = array(
 			array('id' => 1, 'name' => 'joe blow', 'age' => 21),
 			array('id' => 2, 'name' => 'jane blow', 'age' => 21),
@@ -56,6 +56,8 @@ class DataSourceTest extends PHPUnit_Framework_TestCase {
 		);
 
 	}
+
+
 
 }
 
